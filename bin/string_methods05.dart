deaf1b36f23b39f298e1e1c925b04baa1818bcc4
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-31 00:46:12 +0500

    5-misol

--- bin/string_methods05.dart
+++ bin/string_methods05.dart
@@ -1,4 +1,14 @@
 String func(String s, int n) {
+  int b = s.length;
+  if (b == n) {
+    return s;
+  } else {
+    int c;
+    c = n - b;
+    String d = " " * c;
+
+    return s+d;
+  }
   """
     A variable of type str and type of int are given. check that string length is equal to int
     if not equal make it equal with ' ' this string then return that string
@@ -8,8 +18,8 @@ String func(String s, int n) {
     Returns:
         str: answer
     """;
-
-  return '';
 }
 
-void main() {}
+void main() {
+  print(func("oktambek", 23));
+}
